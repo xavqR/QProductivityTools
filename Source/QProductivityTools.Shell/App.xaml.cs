76e74d5adb0058f3ad34424ad88ff8861bc91f30
Javier Quirós de Roa <javiqmail@gmail.com>
2020-05-24 14:04:07 +0200

    Include PRISM

--- Source/QProductivityTools.Shell/App.xaml.cs
+++ Source/QProductivityTools.Shell/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
 
 namespace QProductivityTools
@@ -11,7 +7,17 @@ namespace QProductivityTools
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {       
+            // register other needed services here
+        }
+
+        protected override Window CreateShell()
+        {
+            var w = this.Container.Resolve<MainWindow>();
+            return w;
+        }
     }
 }
